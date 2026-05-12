bd83f5746061a5036fcd9ebef57eaac2afcc1aa1
Muhammad Harun Ar Rasyid <rasyidharunar04@gmail.com>
2026-05-12 14:25:15 +0700

    update aja dulu

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "CONFIG.h"
+#include "RASYID.H"
+
+Editor E;
+
+int main() {
+    setStructEditor(&E);
+    addTab(&E);
+
+    clearScreen();
+    renderHeader(&E);
+
+    while(1) {
+        int c = _getch();
+        TabNode **TT = &E.activeTab;
+        
+        if (c == 0 || c == 224) {
+            c = _getch();
+            arrowKeyHandler(TT, c);
+
+        } else {
+            inputCharHandler(TT, c);
+        }
+    }
+
+    return 0;
+}
